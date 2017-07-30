59637ed967e77368b03e309a50c257e0d17740b5
Erik Andersen Neto <erikneto@br.ibm.com>
2017-07-30 20:17:16 -0300

    teste chatfuel

--- routes/chat.js
+++ routes/chat.js
@@ -2,11 +2,31 @@ const express = require('express');
 const router = express.Router();
 const wcs = require('../helpers/wcs');
 
+let context = {};
+
 router.get('/', (req, res) => {
     res.sendFile('views/chat.html',{root: __dirname.replace('/routes','')});
 });
 
+router.get('/mensagem/:mensagem', (req, res) => {
+    console.dir(req.body);
+    wcs.message({
+        'texto' : req.body.mensagem,
+        'contexto': context
+    },(err,resposta) => {
+        if (err) {
+            console.error(err);
+            res.send(err);
+        }
+        else {
+            res.send({ 'resposta': resposta.output.text[0] ,'context': resposta.context});
+        }
+
+    })    
+});
+
 router.post('/mensagem', (req, res) => {
+    console.dir(req.body);
     wcs.message({
         'texto' : req.body.mensagem,
         'contexto': JSON.parse(req.body.context)
